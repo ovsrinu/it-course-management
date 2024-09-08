cdde83bc5b5135ed3815e59ff300d8d2d8fc0b71
ovsrinu <ovsrinu@gmail.com>
2024-09-08 07:50:07 +0530

    intial commit

---
+++ src/main/java/com/ovsit/ItCoursesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ovsit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItCoursesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItCoursesManagementApplication.class, args);
+	}
+
+}
